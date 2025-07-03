ca0153967c3d82a6754d36e5121299bd18014880
Vitor Mattos <vitorfmattos03@gmail.com>
2025-07-02 21:20:11 -0300

    first commit

---
+++ MediaAlunosPOO/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAlunosPOO
+{
+    internal class Aluno
+    {
+
+        public string Nome { get; set; }
+        public double[] Notas { get; set; }
+        public double Media { get; private set; }
+
+        public Aluno(string nome, int qtdProvas)
+        {
+            Nome = nome;
+            Notas = new double[qtdProvas];
+        }
+
+        public void CalculaMedia()
+        {
+            Console.WriteLine("Insira as notas do aluno " + Nome);
+            double somaNota = 0;
+
+            for(int i = 0; i < Notas.Length; i++)
+            {
+                Console.Write("Nota " + (i+1) + ":");
+                somaNota += double.Parse(Console.ReadLine());
+            }
+
+            Media = somaNota / Notas.Length;
+        }
+    }
+}
---
+++ MediaAlunosPOO/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace MediaAlunosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantos alunos? ");
+            int qtdAlunos = int.Parse(Console.ReadLine());
+
+            Aluno[] alunos = new Aluno[qtdAlunos];
+
+            Console.Clear();
+
+
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+
+                Console.Write("Aluno #" + (i+1) + " Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Aluno #" + (i+1) + " Provas: ");
+                int qtdProvas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, qtdProvas);
+                alunos[i].CalculaMedia();
+
+                Console.Clear();
+
+            }
+
+
+            double somaMedia = 0;
+
+            foreach (Aluno aluno in alunos)
+            {
+
+                Console.WriteLine("Aluno: " + aluno.Nome);
+                Console.WriteLine("Media: " + (aluno.Media).);
+                Console.WriteLine();
+                somaMedia += aluno.Media;
+            }
+
+            Console.WriteLine("Media geral dos alunos: " + (somaMedia / qtdAlunos));
+
+        }
+    }
+}
