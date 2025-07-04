ea62688ac8013069846279f6959ca815031dace6
Vitor Mattos <vitorfmattos03@gmail.com>
2025-07-03 22:59:14 -0300

    Implementado o método de cadastro

--- SistemaCadastroForms/Form1.Designer.cs
+++ SistemaCadastroForms/Form1.Designer.cs
@@ -39,9 +39,9 @@
             this.checkCasa = new System.Windows.Forms.CheckBox();
             this.checkVeiculo = new System.Windows.Forms.CheckBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioM = new System.Windows.Forms.RadioButton();
-            this.radioF = new System.Windows.Forms.RadioButton();
             this.radioO = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.radioM = new System.Windows.Forms.RadioButton();
             this.lista = new System.Windows.Forms.ListBox();
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
@@ -155,16 +155,16 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Sexo";
             // 
-            // radioM
+            // radioO
             // 
-            this.radioM.AutoSize = true;
-            this.radioM.Location = new System.Drawing.Point(11, 34);
-            this.radioM.Name = "radioM";
-            this.radioM.Size = new System.Drawing.Size(89, 20);
-            this.radioM.TabIndex = 0;
-            this.radioM.TabStop = true;
-            this.radioM.Text = "Masculino";
-            this.radioM.UseVisualStyleBackColor = true;
+            this.radioO.AutoSize = true;
+            this.radioO.Location = new System.Drawing.Point(11, 115);
+            this.radioO.Name = "radioO";
+            this.radioO.Size = new System.Drawing.Size(60, 20);
+            this.radioO.TabIndex = 2;
+            this.radioO.TabStop = true;
+            this.radioO.Text = "Outro";
+            this.radioO.UseVisualStyleBackColor = true;
             // 
             // radioF
             // 
@@ -177,16 +177,17 @@
             this.radioF.Text = "Feminino";
             this.radioF.UseVisualStyleBackColor = true;
             // 
-            // radioO
+            // radioM
             // 
-            this.radioO.AutoSize = true;
-            this.radioO.Location = new System.Drawing.Point(11, 115);
-            this.radioO.Name = "radioO";
-            this.radioO.Size = new System.Drawing.Size(60, 20);
-            this.radioO.TabIndex = 2;
-            this.radioO.TabStop = true;
-            this.radioO.Text = "Outro";
-            this.radioO.UseVisualStyleBackColor = true;
+            this.radioM.AutoSize = true;
+            this.radioM.Checked = true;
+            this.radioM.Location = new System.Drawing.Point(11, 34);
+            this.radioM.Name = "radioM";
+            this.radioM.Size = new System.Drawing.Size(89, 20);
+            this.radioM.TabIndex = 0;
+            this.radioM.TabStop = true;
+            this.radioM.Text = "Masculino";
+            this.radioM.UseVisualStyleBackColor = true;
             // 
             // lista
             // 
@@ -206,6 +207,7 @@
             this.btnCadastrar.TabIndex = 12;
             this.btnCadastrar.Text = "Cadastrar / Alterar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
             // btnExcluir
             // 
@@ -215,6 +217,7 @@
             this.btnExcluir.TabIndex = 13;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
             // btnLimpar
             // 
@@ -224,6 +227,7 @@
             this.btnLimpar.TabIndex = 14;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // Form1
             // 
--- SistemaCadastroForms/Form1.cs
+++ SistemaCadastroForms/Form1.cs
@@ -12,14 +12,111 @@ namespace SistemaCadastroForms
 {
     public partial class Form1 : Form
     {
+
+        List<Pessoa> pessoas;
+
         public Form1()
         {
             InitializeComponent();
+
+            pessoas = new List<Pessoa>();
+
+            comboEC.Items.Add("Casado");
+            comboEC.Items.Add("Solteiro");
+            comboEC.Items.Add("Viuvo");
+            comboEC.Items.Add("Separado");
+
+            comboEC.SelectedIndex = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            int index = -1;
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                if(pessoa.Nome == txtNome.Text)
+                {
+                    index = pessoas.IndexOf(pessoa);
+                }
+            }
+
+            if(txtNome.Text == "")
+            {
+                MessageBox.Show("Preencha o campo Nome");
+                txtNome.Focus();
+                return;
+            }
+
+            if(txtTelefone.Text == "")
+            {
+                MessageBox.Show("Preencha o campo Telefone");
+                txtTelefone.Focus();
+                return;
+            }
+
+            char sexo;
+
+            if (radioM.Checked)
+            {
+                sexo = 'M';
+            }
+            else if (radioF.Checked)
+            {
+                sexo = 'F';
+            }
+            else
+            {
+                sexo = 'O';
+            }
+
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.DataNascimento = txtData.Text;
+            p.EstadoCivil = comboEC.SelectedItem.ToString();
+            p.Telefone = txtTelefone.Text;
+            p.CasaPropria = checkCasa.Checked;
+            p.Veiculo = checkVeiculo.Checked;
+            p.Sexo = sexo;
+
+            if(index < 0)
+            {
+                pessoas.Add(p);
+            }
+            else
+            {
+                pessoas[index] = p;
+            }
+
+            btnLimpar_Click(btnLimpar, EventArgs.Empty);
+
+            Listar();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Listar()
+        {
+            lista.Items.Clear();
+
+            foreach (Pessoa p in pessoas)
+            {
+                lista.Items.Add(p.Nome);
+            }
         }
     }
 }
